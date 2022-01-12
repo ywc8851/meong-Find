531c26a314af3381fa5e743eb268c6fd06ef1bdf
sians5544 <tocic@naver.com>
2022-01-12 21:41:39 +0900

    feat : login auth & token setting

--- src/js/requests.js
+++ src/js/requests.js
@@ -10,28 +10,64 @@ export const fetchHtml = async url => {
 };
 
 export const getSignUpForm = async () => {
-  return await axios.get('/signup');
+  try {
+    return await axios.get('/signup');
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getMainPosts = async () => {
-  return await axios.get('/getposts');
+  try {
+    return await axios.get('/getposts');
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getSignUpEmail = async emailValue => {
-  return await axios.get(`/user/email/${emailValue}`);
+  try {
+    return await axios.get(`/user/email/${emailValue}`);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getSignUpName = async nickname => {
-  return await axios.get(`/user/name/${nickname}`);
+  try {
+    return await axios.get(`/user/name/${nickname}`);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // 회원가입 정보 전송
 export const getSignup = async (nickname, email, password, city, district) => {
-  return await axios.post('/users/signup', {
-    nickname,
-    email,
-    password,
-    city,
-    district,
-  });
+  try {
+    return await axios.post('/users/signup', {
+      nickname,
+      email,
+      password,
+      city,
+      district,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postSignIn = async (email, password, autoLogin) => {
+  try {
+    return await axios.post('/user/signin', { email, password, autoLogin });
+  } catch (error) {
+    return console.error(error);
+  }
+};
+
+export const getSignOut = async () => {
+  try {
+    return await axios.get('/user/signout');
+  } catch (error) {
+    console.error(error);
+  }
 };
