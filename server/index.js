a698d36ef39c94ae21cd8226b805ace66f315d00
sians5544 <tocic@naver.com>
2022-01-14 20:29:13 +0900

    refactor : signin refactoring &  add mail file

--- server/index.js
+++ server/index.js
@@ -8,13 +8,15 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const { auth, blockLoginUser } = require('./auth.js');
 const { users, posts, comments } = require('./db');
 
+const { emailOptions, transporter } = require('./mail.js');
+
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
 
 const config = require('../webpack.config.js');
 const compiler = webpack(config);
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
 app.use(webpackDevMiddleware(compiler));
 
@@ -23,23 +25,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 const createToken = (email, expirePeriod) => jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: expirePeriod });
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.ADMIN_EMAIL,
-    pass: process.env.ADMIN_PASSWORD,
-  },
-});
-
-const emailOptions = {
-  from: process.env.ADMIN_EMAIL,
-  to: '',
-  subject: '임시 비밀번호 안내 입니다.',
-  html: '',
-};
 
 const urls = ['/signin', '/signup', '/detail', '/mypage', '/mypageEdit'];
 
@@ -199,7 +184,8 @@ app.get('/user/email/:email', (req, res) => {
 //로그인
 app.post('/user/signin', (req, res) => {
   const { email, password, autoLogin } = req.body;
-  const [user] = users.filter({ email, password });
+  const [user] = users.filter({ email, password, isValid: true });
+
   if (!user) {
     return res.status(401).send('등록되지 않은 사용자입니다.');
   }
@@ -211,10 +197,7 @@ app.post('/user/signin', (req, res) => {
     httpOnly: true,
   });
 
-  // 아이디 필요한지 고뇌
-  res.send({
-    id: user.id,
-  });
+  res.send();
 });
 
 //로그아웃
@@ -225,7 +208,7 @@ app.get('/user/signout', (req, res) => {
 // 존재하는 이메일인지 확인
 app.get('/user/id/:email', (req, res) => {
   const { email } = req.params;
-  const [user] = users.filter({ email });
+  const [user] = users.filter({ email, isValid: true });
 
   res.send({
     id: user.id,
@@ -237,9 +220,10 @@ app.patch('/user/temporary', (req, res) => {
   const { id, password } = req.body;
   const updatedUser = users.update(id, { password });
 
-  if (updatedUser) {
+  if (!updatedUser) {
     return res.status(401).send('임시비밀번호 변경에 실패 했습니다.');
   }
+
   emailOptions.to = updatedUser.email;
   emailOptions.html = `
   <h2>임시 비밀번호 안내 입니다.</h2>
@@ -248,10 +232,7 @@ app.patch('/user/temporary', (req, res) => {
 
   transporter.sendMail(emailOptions);
 
-  // 이거 꼭 필요한지 체크
-  res.send({
-    updatedUser,
-  });
+  res.send();
 });
 
 app.get('/user/login', auth);
--- src/js/pages/signIn.js
+++ src/js/pages/signIn.js
@@ -16,12 +16,11 @@ const handlePopup = () => {
 };
 
 const randomPasssword = () => {
-  // 인덱스 -1 접근불가하게끔 만들기
   let setStr = '0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'.split(',');
   let randomStr = '';
 
   for (let i = 0; i < 8; i++) {
-    randomStr += setStr[Math.floor(Math.random() * setStr.length - 1)];
+    randomStr += setStr[Math.floor(Math.random() * (setStr.length - 1))];
   }
 
   return randomStr;
@@ -60,38 +59,26 @@ const bindEvents = () => {
   });
 
   $('.sign-in-form').addEventListener('input', e => {
-    // 삼항으로 바꾸세용
-    if (e.target.matches('#email')) {
-      validate.emailValidate(e.target.value, 0, $signinbtn);
-    } else if (e.target.matches('#password')) {
-      validate.passwordValidate(e.target.value, 1, $signinbtn);
-    }
+    e.target.matches('#email')
+      ? validate.emailValidate(e.target.value, 0, $signinbtn)
+      : validate.passwordValidate(e.target.value, 1, $signinbtn);
   });
 
   $('.popup-form').addEventListener('input', e => {
-    const regEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
-
-    regEmail.test(e.target.value)
+    validate.regEmail.test(e.target.value)
       ? $('.popup-button').removeAttribute('disabled')
       : $('.popup-button').setAttribute('disabled', '');
   });
 
   $('.popup-button').addEventListener('click', async e => {
-    // 1.현재 등록되어있는 이메일 인지 확인해야됨
-    // 2-1 등록되어있는 경우 임시 비밀번호 발급해서 그걸로 교체 쳐줌
-    // 2-2 안등록이면 다시 입력하라고 오류 문구 출력
     e.preventDefault();
 
     try {
       const user = await getUserId($('#check-password').value.trim());
+      const pwd = randomPasssword().trim();
 
-      const checkUser = user.data.id;
-
-      if (checkUser) {
-        const pwd = randomPasssword().trim();
-        const updatedUser = await changePassword(checkUser, pwd);
-        if (updatedUser) alert('메일 발송이 완료되었습니다.');
-      }
+      await changePassword(user.data.id, pwd);
+      alert('메일 발송이 완료되었습니다.');
       handlePopup();
     } catch (error) {
       console.error(error);
